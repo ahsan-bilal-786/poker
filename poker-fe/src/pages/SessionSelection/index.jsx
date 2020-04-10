172f845e01330e21dca86743e02bd10f3781800a
ahsan-bilal-786 <ahsan.bilal@arbisoft.com>
2020-04-10 23:35:24 +0500

    adding poker fe

---
+++ poker-fe/src/pages/SessionSelection/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SessionSelection extends Component {
+  render() {
+    return <div>HI this is session selection</div>;
+  }
+}
+
+export default SessionSelection;
