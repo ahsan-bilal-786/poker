e614bd3020748cdee67dfece41e1edd38f8defa2
ahsan-bilal-786 <engr.ahsan.bilal@gmail.com>
2020-04-15 00:15:21 +0500

    Updates

---
+++ precache-manifest.52a45199eddd04005f45c8cca06d631c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b53934b0c060881b07eed02f047be7",
+    "url": "/poker/index.html"
+  },
+  {
+    "revision": "c2b689b3dfdd97abbd3d",
+    "url": "/poker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "22f90d0f52d00be3e922",
+    "url": "/poker/static/css/main.5337afa8.chunk.css"
+  },
+  {
+    "revision": "c2b689b3dfdd97abbd3d",
+    "url": "/poker/static/js/2.7e8a3b50.chunk.js"
+  },
+  {
+    "revision": "53e943b2733f1787c5b3bfc6fe3aac25",
+    "url": "/poker/static/js/2.7e8a3b50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22f90d0f52d00be3e922",
+    "url": "/poker/static/js/main.fd8489b1.chunk.js"
+  },
+  {
+    "revision": "15a945513e60dd4bde1b",
+    "url": "/poker/static/js/runtime-main.a43d0e32.js"
+  },
+  {
+    "revision": "ed7886ec3be9f6a97b6c078addf2ccfe",
+    "url": "/poker/static/media/0 - planning poker_Here be dragons.ed7886ec.svg"
+  },
+  {
+    "revision": "564830ebed798205970ce2850a21938c",
+    "url": "/poker/static/media/0.5 - planning poker_Coffee break.564830eb.svg"
+  },
+  {
+    "revision": "1c7bf067d768f153f448683f2a146801",
+    "url": "/poker/static/media/1 - planning poker_Low hanging fruit.1c7bf067.svg"
+  },
+  {
+    "revision": "ef2886c575a5be8876c0b01fa72cdc8f",
+    "url": "/poker/static/media/100 - planning poker_Monster task.ef2886c5.svg"
+  },
+  {
+    "revision": "2ef1fb740da793f718a11da535513424",
+    "url": "/poker/static/media/101 - planning poker_When pigs fly.2ef1fb74.svg"
+  },
+  {
+    "revision": "16839e4321dfa23bc027e36cd002d9fd",
+    "url": "/poker/static/media/13 - planning poker_Squeaking by.16839e43.svg"
+  },
+  {
+    "revision": "5ff0669a0240b57660d187d82fb722ee",
+    "url": "/poker/static/media/2 - planning poker_Piece of cake.5ff0669a.svg"
+  },
+  {
+    "revision": "2db23d77949185fe271cd0e154a8adf5",
+    "url": "/poker/static/media/20 - planning poker_Don't put all .2db23d77.svg"
+  },
+  {
+    "revision": "d4128a505111319ad07afbfd0bb8b424",
+    "url": "/poker/static/media/3 - planning poker_It ain't rocket science.d4128a50.svg"
+  },
+  {
+    "revision": "71045c711685e5c28e3ee623267eed13",
+    "url": "/poker/static/media/40 - planning poker_Meterse en un berenjenal.71045c71.svg"
+  },
+  {
+    "revision": "c42e9a40ddbba64d3d86ba3dc7ad60b7",
+    "url": "/poker/static/media/5 - planning poker-Ornitorinco.c42e9a40.svg"
+  },
+  {
+    "revision": "e7cdcc87e0a0ac6a81feae4a64d52a14",
+    "url": "/poker/static/media/8 - planning poker_An arm and a leg.e7cdcc87.svg"
+  },
+  {
+    "revision": "6164385c357df75e2b38946d1fc7a501",
+    "url": "/poker/static/media/lg - planning poker_An arm and a leg.6164385c.svg"
+  },
+  {
+    "revision": "d076828178355359bb6a5db81c3ba65d",
+    "url": "/poker/static/media/md - planning poker_Squeaking by.d0768281.svg"
+  },
+  {
+    "revision": "fc9a4c3ec43e1b46b272d18f4fb8fb75",
+    "url": "/poker/static/media/planning poker_Eat a brownie.fc9a4c3e.svg"
+  },
+  {
+    "revision": "405c5c43c8acbe210302b11d292c97e5",
+    "url": "/poker/static/media/sm - planning poker_Don't put all.405c5c43.svg"
+  },
+  {
+    "revision": "7a8b40305c53b47e0acceda608ffcece",
+    "url": "/poker/static/media/xl - planning poker_Low hanging fruit.7a8b4030.svg"
+  },
+  {
+    "revision": "07fe1f8957b16df6748e8c0488f4db0c",
+    "url": "/poker/static/media/xs - planning poker_When pigs fly.07fe1f89.svg"
+  },
+  {
+    "revision": "d0736c840e6eabab5f8fd970aecd0985",
+    "url": "/poker/static/media/xxl - planning poker_It ain't rocket science.d0736c84.svg"
+  }
+]);
