b9167a4ce71c9703c406ff99f8b40b9a3473530f
ahsan-bilal-786 <engr.ahsan.bilal@gmail.com>
2020-04-22 23:40:32 +0500

    till now, we have an api which fetches the data from server, actions to dispatch the data and reducer to store the data in redux, but an action should be called from any component point to execute this complete flow, so fetchPollTypesListAction has been passed to component as props and component is calling this function from useEffect function. after these implementation you can see the data of in redux dev tools.

--- poker-fe/src/pages/SessionSelection/index.jsx
+++ poker-fe/src/pages/SessionSelection/index.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { setPollType, setSessionId } from "store/actions/polling";
+import {
+  setPollType,
+  setSessionId,
+  fetchPollTypesListAction,
+} from "store/actions/polling";
 import { Row, Col } from "react-bootstrap";
 import JoinSessionCard from "components/SessionSelection/JoinSession";
 import CreateSessionCard from "components/SessionSelection/CreateSession";
 
-const SessionSelection = ({ history, setPollType, setSessionId }) => {
+const SessionSelection = ({
+  history,
+  setPollType,
+  setSessionId,
+  fetchPollTypesList,
+}) => {
+  useEffect(() => {
+    fetchPollTypesList();
+  }, [fetchPollTypesList]);
   return (
-    <Row className='align-items-center h-100 mt-5'>
-      <Col sm='6'>
+    <Row className="align-items-center h-100 mt-5">
+      <Col sm="6">
         <CreateSessionCard
           history={history}
           setPollType={setPollType}
           setSessionId={setSessionId}
         />
       </Col>
-      <Col sm='6 '>
+      <Col sm="6 ">
         <JoinSessionCard history={history} setSessionId={setSessionId} />
       </Col>
     </Row>
@@ -26,6 +38,7 @@ const mapDispatchToProps = (dispatch) => {
   return {
     setPollType: (pollType) => dispatch(setPollType(pollType)),
     setSessionId: (sessionId) => dispatch(setSessionId(sessionId)),
+    fetchPollTypesList: () => dispatch(fetchPollTypesListAction()),
   };
 };
 
