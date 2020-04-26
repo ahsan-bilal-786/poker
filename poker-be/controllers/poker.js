702cafea27d33f5660460f0a4243313bff245e65
ahsan-bilal-786 <engr.ahsan.bilal@gmail.com>
2020-04-26 23:34:38 +0500

    removing useless consoles

--- poker-be/controllers/poker.js
+++ poker-be/controllers/poker.js
@@ -1,13 +1,11 @@
 var { Session, SessionType, Pollings } = require("../models");
 
 const saveSession = (req, res, next) => {
-  console.log("================================", req.body);
   return Session.create(req.body)
     .then((data) => {
       return res.json(data);
     })
     .catch((err) => {
-      console.log("================================", err.message);
       res.status(500).send({
         message:
           err.message || "Some error occurred while creating the Session.",
