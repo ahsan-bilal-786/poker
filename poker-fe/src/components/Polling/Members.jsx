a0862b6523bf6e0c12ba2b6ef9ab84a2a3dbf4a3
ahsan-bilal-786 <engr.ahsan.bilal@gmail.com>
2020-04-26 23:08:55 +0500

    updating card keys

--- poker-fe/src/components/Polling/Members.jsx
+++ poker-fe/src/components/Polling/Members.jsx
@@ -4,19 +4,19 @@ import { Card, ListGroup } from "react-bootstrap";
 const MembersList = ({ polls }) => {
   let usersList = [];
   if (Array.isArray(polls) && polls.length > 0)
-    usersList = polls.map(({ userName, card }, i) => {
+    usersList = polls.map(({ userName, poll }, i) => {
       return (
-        <ListGroup.Item key={i}>
-          {card === "" ? (
+        <ListGroup.Item key={poll.id}>
+          {poll === "" ? (
             <span
-              className='text-warning mr-2'
-              role='img'
-              aria-label='exclamation'
+              className="text-warning mr-2"
+              role="img"
+              aria-label="exclamation"
             >
               &#10069;
             </span>
           ) : (
-            <span className='text-success mr-2' role='img' aria-label='tick'>
+            <span className="text-success mr-2" role="img" aria-label="tick">
               &#10004;
             </span>
           )}
@@ -26,11 +26,11 @@ const MembersList = ({ polls }) => {
     });
   return (
     <Card>
-      <Card.Header as='h5' className='text-center'>
+      <Card.Header as="h5" className="text-center">
         Members
       </Card.Header>
-      <Card.Body className='p-0 members-list'>
-        <ListGroup variant='flush'>{usersList}</ListGroup>
+      <Card.Body className="p-0 members-list">
+        <ListGroup variant="flush">{usersList}</ListGroup>
       </Card.Body>
     </Card>
   );
